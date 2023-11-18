180a5336be4fecdd25cc69ce9a8f8b308cc3af4c
ashi136 <87899037+ashi136@users.noreply.github.com>
2023-11-18 11:11:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home'
 import List from './components/List'
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/nobel-prize-list">
     <div>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/list' element={<List />} />
       </Routes>
     </div>
-  </Router>
+  </BrowserRouter>
   );
 }
-
-export default App;
